8c9930b485e8e9f9dde86779860fda36e432c680
gaoqiang19514 <gaoqiang19514@163.com>
2016-11-05 17:28:15 +0800

    update

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@
 	var controlList = $('.control-list-item');
 	var contentList = $('.content-list-item');
 
-
 	controlList.click(function(event) {
 		var index = $(this).index();
 
@@ -11,12 +10,8 @@
 
 		contentList.eq(index).slideDown('slow').siblings().slideUp('fast');
 
-
-
 	});
 })();
-
-
 
 // 图片轮播 start
 (function(){
@@ -33,7 +28,6 @@ function Carousel(){
 	this.listIndexArray = this.carousel.find('.list-index-item');
 	this.singleCarouselItemWidth = $(this.carouselArray[0]).width();
 
-
 	// 默认需要从第二索引开始，因为索引0的元素为轮播倒退到顶部才显示
 	this.carouselIndex = 1;
 
@@ -47,12 +41,10 @@ function Carousel(){
 	this.listIndexArray.click(function(event) {
 		var current = $(this).index() + 1;
 
-
 		if(!$(_this.carouselInner).is(':animated')){
 			$(_this.carouselInner).animate({left : -(current * _this.singleCarouselItemWidth)}, 500);
 			_this.carouselIndex = current;
 			_this.changeCurrentIndexBtn();
-			console.log('12');
 		}
 
 	});
@@ -70,8 +62,6 @@ function Carousel(){
 		}
 		_this.move('right');
 	});
-
-
 }
 Carousel.prototype = {
 	init : function(){
@@ -124,21 +114,10 @@ Carousel.prototype = {
 
 };
 
-
 new Carousel().init();
 
 })();
 // 图片轮播 end
-
-
-
-
-
-
-
-
-
-
 
 // 元素拖曳 start
 window.onload = function(){
@@ -189,8 +168,6 @@ window.onload = function(){
 
 				}
 			}
-
-
 		}
 		MoveElement.prototype = {
 			getElementsByClassName : function(className, parent){
@@ -217,3 +194,71 @@ window.onload = function(){
 }
 
 // 元素拖曳 end
+
+
+$.prototype.tabs = function(options){
+	this.each(function(index, el) {
+		new Tabs($(el), options);
+	});
+
+	return this;
+};
+
+function Tabs($elem, options){
+	var that    = this;
+
+	var defaults = {
+	    cIndex		: 0,             			//当前显示tab名称
+	    tabNavs: '.tab__navs',
+	    tabTabs: '.tab__tabs',
+	    tabLinkClass: '.tab__link',
+	    tabItemClass: '.tab__item',
+	    activeClass : 'active'      			//当tab被选择时的样式
+	};
+
+	this.options = $.extend({}, defaults, options); //初始化参数
+	this.$elem   = $elem;
+	this.$navs	 = this.$elem.children(this.options.tabNavs);
+	this.$tabs   = this.$elem.children(this.options.tabTabs);
+
+	this.init();
+}
+
+Tabs.prototype.init = function(){
+	//	初始化显示
+	this.show(this.options.cIndex);
+
+	this.events();
+};
+
+Tabs.prototype.events = function(){
+	var that = this;
+
+	this.$navs.on('click', this.options.tabLinkClass, function(){
+		var index = $(this).index();
+		that.show(index);
+	});
+};
+
+Tabs.prototype.show = function(index){
+	//	判断上一次点击和当前点击的是否为同一元素,如果是直接返回
+	if(this.cIndex === index){
+		return; 
+	};
+	//	如果点的是新元素，则把之前的隐藏掉
+	this.hide(this.cIndex);
+	//	把当前点击的下标缓存起来
+	this.cIndex = index;
+	//	显示当前元素
+	this.$tabs.children(this.options.tabItemClass).eq(index).addClass(this.options.activeClass);
+	this.$navs.children(this.options.tabLinkClass).eq(index).addClass(this.options.activeClass);
+};
+
+Tabs.prototype.hide = function(index){
+	this.$tabs.children(this.options.tabItemClass).eq(index).removeClass(this.options.activeClass);
+	this.$navs.children(this.options.tabLinkClass).eq(index).removeClass(this.options.activeClass);
+};
+
+$('.tab').tabs({
+	cIndex: 0
+});
